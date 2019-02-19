842a86d93f5241c37357f0bf3dec29814a7fe6e9
sam <9823538@qq.com>
2019-02-19 10:32:27 +0800

    changed

--- kejike_remove.py
+++ kejike_remove.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 from pymongo import MongoClient
 import time
 import pprint
@@ -6,42 +7,38 @@ host = '192.168.18.61'
 port = '27017'
 user = ''
 password = ''
+
 if user.strip() == "":
     uri = "mongodb://%s:%s" % (host, port)
 else:
     uri = "mongodb://%s:%s@%s:%s" % (user, password, host, port)
+
 numMonthes = 8    # how many months to keep
-
 dbs = 'face'
+collection = 'passerby'
 
-collections = ['passerby',]
-
-
-def timeKeep(numMonthes):
+def time_keep(numMonthes):
     now = int(time.time())    # current time
     oneMonth = 60 * 60 * 24 * 30    # timestamps in one month
     t = now - numMonthes * oneMonth    # remove the data before this time
     return t
 
-
-def get_db(dbs):
+def get_collection():
     client = MongoClient(uri)
     db = client[dbs]
-    return db
-
-
-timeKeep = timeKeep(numMonthes)   # timestamp
-
-print("timestamp:", timeKeep)
-
-db = get_db(dbs)
-
-for collection in collections:
     collection = db[collection]
-    print("start cleaning collection %s" % (collection.name))
-    count = collection.count_documents({'create_time': {"$lt": timeKeep}})
-    print(count)
-    result = collection.find({'create_time': {"$lt": timeKeep}})
-    pprint.pprint(result[4])
-#    print("%d documents removed in %s" % (result, collection.name))
+    return collection
 
+collection =  get_collection
+timeKeep = time_keep(numMonthes)
+count = collection.count_documents({'create_time': {"$lt": timeKeep}})
+
+print("start cleaning collection, %d documents in %d monthes will be removed in collection %s" % (count, numMonthes, collection.name))
+
+result = collection.find({'create_time': {"$lt": timeKeep}})
+for r in result:
+   if 'back_pic_src' in r:
+       pprint.pprint(r['back_pic_src'])
+   else:
+       print('not exist')
+#  print("%d documents removed in %s" % (result, collection.name))
